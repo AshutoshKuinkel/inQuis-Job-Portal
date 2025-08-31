c4b773dd47fd2b2882cf7cc7bf7217d10aef6b55
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-08-31 11:33:15 +1000

    sending token to local storage

--- Frontend/src/components/auth components/login.form.tsx
+++ Frontend/src/components/auth components/login.form.tsx
@@ -30,6 +30,7 @@ const LoginForm = () => {
     mutationFn: loginAPI,
     onSuccess: (response) => {
       console.log(response);
+      console.log("Token:", response.inQuis_portal_accessToken);
       toast.success(response?.message ?? "Login Success", {
         style: {
           border: " 1px solid #2c3e50",
@@ -41,7 +42,7 @@ const LoginForm = () => {
         },
       });
       localStorage.setItem('user',JSON.stringify(response.data))
-      localStorage.setItem('token',response.data.inQuis_portal_accessToken)
+      localStorage.setItem('token',response.inQuis_portal_accessToken)
       setTimeout(()=>navigate('/'),1000)
     },
     onError: (error) => {
