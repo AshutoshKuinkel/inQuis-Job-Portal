53808633b74f93c3e8ef0c438f271624b7058440
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-04 17:25:04 +1000

    ...

--- Frontend/src/components/list jobs page/job cards/index.tsx
+++ Frontend/src/components/list jobs page/job cards/index.tsx
@@ -4,20 +4,20 @@ import JobCard from "./job-main-card";
 import { getAllJobsAPI } from "../../../api/job.api";
 import { IJob } from "../../../types/job.types";
 import { useNavigate, useParams } from "react-router";
+import { Oval } from "react-loading-icons";
 
 const JobDisplay = () => {
+  const navigate = useNavigate();
+  const { id: selectedJobId } = useParams();
 
-  const navigate = useNavigate()
-  const {id: selectedJobId} = useParams()
+  const { data, isLoading } = useQuery({
+    queryFn: getAllJobsAPI,
+    queryKey: ["get_all_jobs"],
+  });
 
-  const {data} = useQuery({
-    queryFn:getAllJobsAPI,
-    queryKey:['get_all_jobs']
-  })
-
-  const handleJobClick=(id:string)=>{
-    navigate(`/jobs/${id}`)
-  }
+  const handleJobClick = (id: string) => {
+    navigate(`/jobs/${id}`);
+  };
 
   return (
     <div className="flex justify-center mt-16">
@@ -25,20 +25,31 @@ const JobDisplay = () => {
         {/* Cards */}
         <div className="col-span-1">
           <div className="flex flex-col gap-3">
-            {data?.data.map((job:IJob)=>(
-              <JobCard job={job} key={job._id} handleClick={handleJobClick}/>
-            ))}
+            {isLoading ? (
+              <div className="flex justify-center items-center col-span-4 h-[300px]">
+                <Oval stroke="#2c3e50" height="64" width="64" />
+              </div>
+            ) : (
+              data?.data.map((job: IJob) => (
+                <JobCard job={job} key={job._id} handleClick={handleJobClick} />
+              ))
+            )}
           </div>
         </div>
 
         {/* Detail Section */}
-        <div className="col-span-2 border max-w-4xl h-screen sticky top-0">
-          <div className="h-full overflow-hidden hover:overflow-auto"
-           style={{ scrollbarGutter: "stable" }}
-          >
-            <DetailCard jobId={selectedJobId ?? null}/>
+        {isLoading ? (
+          ""
+        ) : (
+          <div className="col-span-2 border max-w-4xl h-screen sticky top-0">
+            <div
+              className="h-full overflow-hidden hover:overflow-auto"
+              style={{ scrollbarGutter: "stable" }}
+            >
+              <DetailCard jobId={selectedJobId ?? null} />
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
