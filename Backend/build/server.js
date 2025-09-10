429c095cee648b91d8e6935ddb06ab7ca1a68c5e
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-10 21:21:29 +1000

    origin added cors server ts

--- Backend/build/server.js
+++ Backend/build/server.js
@@ -50,6 +50,7 @@ const DB_URI = process.env.DB_URI ?? '';
 const app = (0, express_1.default)();
 app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)({
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 app.use(express_1.default.json()); // for JSON bodies
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -15,6 +15,7 @@ const app = express()
 
 app.use(helmet())
 app.use(cors({
+  origin:'http://localhost:5173',
   credentials:true
 }))
 app.use(express.json());        // for JSON bodies
