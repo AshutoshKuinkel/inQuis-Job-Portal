27f1529e0c51a10a2592b8afd14fc3bb016a80bf
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-10 20:38:52 +1000

    removed seeker role for application route

--- Backend/build/routes/application.routes.js
+++ Backend/build/routes/application.routes.js
@@ -11,7 +11,7 @@ const enum_types_2 = require("../types/enum.types");
 const uploader_middleware_1 = require("../middlewares/uploader.middleware");
 const router = express_1.default.Router();
 const upload = (0, uploader_middleware_1.uploader)();
-router.post('/apply/:jobId', (0, auth_middleware_1.authenticate)(enum_types_1.seeker), upload.fields([{ name: 'resume', maxCount: 1 }]), application_controller_1.apply);
+router.post('/apply/:jobId', (0, auth_middleware_1.authenticate)(), upload.fields([{ name: 'resume', maxCount: 1 }]), application_controller_1.apply);
 router.get('/myApplications', (0, auth_middleware_1.authenticate)(enum_types_1.seeker), application_controller_1.viewMyApplications);
 router.put('/updateApplication/:jobId', (0, auth_middleware_1.authenticate)(enum_types_1.seeker), application_controller_1.update);
 router.delete('/withdrawApplication/:jobId', (0, auth_middleware_1.authenticate)(enum_types_1.seeker), application_controller_1.withdraw);
--- Backend/src/routes/application.routes.ts
+++ Backend/src/routes/application.routes.ts
@@ -8,7 +8,7 @@ import { uploader } from '../middlewares/uploader.middleware';
 const router = express.Router()
 const upload = uploader()
 
-router.post('/apply/:jobId',authenticate(seeker),upload.fields([{name:'resume',maxCount:1}]),apply)
+router.post('/apply/:jobId',authenticate(),upload.fields([{name:'resume',maxCount:1}]),apply)
 router.get('/myApplications',authenticate(seeker),viewMyApplications)
 router.put('/updateApplication/:jobId',authenticate(seeker),update)
 router.delete('/withdrawApplication/:jobId',authenticate(seeker),withdraw)
