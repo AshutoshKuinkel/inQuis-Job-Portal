37399b39e3564360aca082ac1696019e4c939bc3
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-10 19:22:30 +1000

    ...

--- Frontend/src/api/apply.api.ts
+++ Frontend/src/api/apply.api.ts
@@ -1,11 +1,15 @@
-import { IApplicationData } from '../types/application.types'
-import api from './'
+import { IApplicationData } from "../types/application.types";
+import api from "./";
 
-export const applicationAPI = async(id:string,data:IApplicationData)=>{
-  try{
-    const response = await api.post(`/jobs/apply/${id}`,data)
-    return response.data
-  }catch(err:any){
-    throw err.response.data
+export const applicationAPI = async (id: string, data: IApplicationData) => {
+  try {
+    const response = await api.post(`/jobs/apply/${id}`, data);
+    return response.data;
+  } catch (err: any) {
+    console.log("ERR FULL:", err);
+    console.log("ERR RESPONSE:", err.response?.data);
+    console.log("ERR MESSAGE:", err.message);
+
+    throw err; // rethrow the full Axios error, not just err.response.data
   }
-}
+};
