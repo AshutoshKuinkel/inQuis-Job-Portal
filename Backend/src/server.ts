e765c99f1beb9d330b62b7261bd21d3066713604
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-10 21:25:22 +1000

    new lines added for cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -14,13 +14,25 @@ ConnectDatabase(DB_URI)
 const app = express()
 
 app.use(helmet())
+app.use(cookieParser())
+
+const allowedOrigins = ['http://localhost:5173','https://inquis-portal.vercel.app']
+
 app.use(cors({
-  origin:'http://localhost:5173',
-  credentials:true
-}))
+  origin: function(origin, callback) {
+    // Allow requests with no origin (like Postman or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'CORS policy does not allow access from this origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // <--- This sets Access-Control-Allow-Credentials: true
+}));
+
 app.use(express.json());        // for JSON bodies
 app.use(express.urlencoded({ extended: true })); // for form-urlencoded bodies
-app.use(cookieParser())
 
 //serving uploads as static files:
 app.use('/uploads',express.static('uploads/'))
