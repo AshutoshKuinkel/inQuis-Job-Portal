37dcabda7c9f1671c31d1b625ac9137e2e2eef49
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-10 18:38:22 +1000

    debugging

--- Backend/build/middlewares/error-handler.middleware.js
+++ Backend/build/middlewares/error-handler.middleware.js
@@ -9,7 +9,7 @@ class CustomError extends Error {
     }
 }
 const errorHandler = async (err, req, res, next) => {
-    const message = err?.message || 'Internal Server Error.';
+    const message = err.message || 'Internal Server Error.';
     const statusCode = err?.statusCode || 500;
     res.status(statusCode).json({
         message: message,
--- Backend/src/middlewares/error-handler.middleware.ts
+++ Backend/src/middlewares/error-handler.middleware.ts
@@ -14,7 +14,7 @@ class CustomError extends Error{
 
 export const errorHandler = async(err:any,req:Request,res:Response,next:NextFunction)=>{
 
-  const message = err ?.message || 'Internal Server Error.'
+  const message = err.message || 'Internal Server Error.'
   const statusCode = err ?.statusCode || 500
 
   res.status(statusCode).json({
