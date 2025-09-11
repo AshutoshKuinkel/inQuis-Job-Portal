619beded7ab5682c0b4f46494d5bd3dc20c96b8e
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-11 16:05:13 +1000

    reverted back to original auth middleware

--- Backend/build/middlewares/auth.middleware.js
+++ Backend/build/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 "use strict";
-// //get cookie response, {just get our access token and if we don't have an access token return access denied unauthorised. This is to restrict somethings to logged in users only.}
+//get cookie response, {just get our access token and if we don't have an access token return access denied unauthorised. This is to restrict somethings to logged in users only.}
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,70 +8,36 @@ exports.authenticate = void 0;
 const error_handler_middleware_1 = __importDefault(require("./error-handler.middleware"));
 const jwt_utils_1 = require("../utils/jwt.utils");
 const user_model_1 = require("../models/user.model");
-// //verify token, {add the verify function to jwt utils.} {do the ijwtdecodedpayload which extends ijwtpayload and adds, iat, exp.} {make sure the token is not expired. }
-// //check that the user exists using the user model and passing the decodedData._id
-// //roles includes.... {}
-// //
-// export const authenticate = (roles?: Role[]) => {
-//   return async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       //getting the token:
-//       const inQuis_portal_accessToken = req.cookies.inQuis_portal_accessToken;
-//       if (!inQuis_portal_accessToken) {
-//         throw new CustomError(`401 code 5`, 401);
-//       }
-//       //verifying token:
-//       const decodedData = verifyAccessToken(inQuis_portal_accessToken);
-//       if (Date.now() > decodedData.exp * 1000) {
-//         res.clearCookie("inQuis_portal_accessToken", {
-//           secure: process.env.NODE_ENV === "development" ? false : true,
-//           httpOnly: true,
-//           sameSite: "none",
-//         });
-//         throw new CustomError(`401 code 6`, 401);
-//       }
-//       //checking user exists.
-//       const user = await User.findById(decodedData._id);
-//       if (!user) {
-//         throw new CustomError(`401 code 7`, 401);
-//       }
-//       //role based authorisation:
-//       if (roles && !roles.includes(decodedData.role)) {
-//         throw new CustomError(`Unauthorised. Access Denied.`, 403);
-//       }
-//       req.user = {
-//         _id: decodedData._id,
-//         email: decodedData.email,
-//         role: decodedData.role,
-//         first_name: decodedData.first_name,
-//         last_name: decodedData.last_name,
-//       };
-//       next();
-//     } catch (err) {
-//       console.error("AUTH ERROR:", err); // add this
-//       next(err);
-//     }
-//   };
-// };
+//verify token, {add the verify function to jwt utils.} {do the ijwtdecodedpayload which extends ijwtpayload and adds, iat, exp.} {make sure the token is not expired. }
+//check that the user exists using the user model and passing the decodedData._id
+//roles includes.... {}
+//
 const authenticate = (roles) => {
     return async (req, res, next) => {
-        const inQuis_portal_accessToken = req.cookies.inQuis_portal_accessToken;
-        if (!inQuis_portal_accessToken) {
-            console.log("NO TOKEN FOUND");
-            return next(new error_handler_middleware_1.default("401 code 5", 401));
-        }
         try {
+            //getting the token:
+            const inQuis_portal_accessToken = req.cookies.inQuis_portal_accessToken;
+            if (!inQuis_portal_accessToken) {
+                throw new error_handler_middleware_1.default(`Unauthorised. Access denied.`, 401);
+            }
+            //verifying token:
             const decodedData = (0, jwt_utils_1.verifyAccessToken)(inQuis_portal_accessToken);
             if (Date.now() > decodedData.exp * 1000) {
-                res.clearCookie("inQuis_portal_accessToken");
-                return next(new error_handler_middleware_1.default("401 code 6", 401));
+                res.clearCookie("inQuis_portal_accessToken", {
+                    secure: process.env.NODE_ENV === "development" ? false : true,
+                    httpOnly: true,
+                    sameSite: "none",
+                });
+                throw new error_handler_middleware_1.default(`Unauthorised. Access denied.`, 401);
             }
+            //checking user exists.
             const user = await user_model_1.User.findById(decodedData._id);
             if (!user) {
-                return next(new error_handler_middleware_1.default("401 code 7", 401));
+                throw new error_handler_middleware_1.default(`Unauthorised. Access denied.`, 401);
             }
+            //role based authorisation:
             if (roles && !roles.includes(decodedData.role)) {
-                return next(new error_handler_middleware_1.default("403 code 1", 403));
+                throw new error_handler_middleware_1.default(`Unauthorised. Access Denied.`, 403);
             }
             req.user = {
                 _id: decodedData._id,
@@ -83,8 +49,8 @@ const authenticate = (roles) => {
             next();
         }
         catch (err) {
-            console.log("VERIFY ERROR:", err);
-            return next(new error_handler_middleware_1.default("401 code 8", 401));
+            console.error("AUTH ERROR:", err); // add this
+            next(err);
         }
     };
 };
