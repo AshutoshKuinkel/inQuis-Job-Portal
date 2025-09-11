b14817ab8ea6c737586db8a41c4477d4b79bc0c3
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-11 12:01:04 +1000

    origin true removed from cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -17,7 +17,6 @@ app.use(helmet())
 app.use(cookieParser())
 app.use(cors({
   credentials:true,
-  origin:true
 }))
 app.use(express.json());        // for JSON bodies
 app.use(express.urlencoded({ extended: true })); // for form-urlencoded bodies
