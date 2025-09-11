f80904cc9d099990a7118ea1058d80ef5444de4b
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-11 14:56:39 +1000

    authorisation + authentication HOC implemented

---
+++ Frontend/src/hoc/with-auth.hoc.tsx
@@ -0,0 +1,51 @@
+import { ComponentType } from "react";
+import { useAuth } from "../context/auth-context";
+import { Oval } from "react-loading-icons";
+import { Navigate } from "react-router";
+import toast from "react-hot-toast";
+import { Role } from "../types/enum.types";
+
+export function withAuth<T>(Component: ComponentType<T>,roles:Role[]) {
+  return function ProtectedComponent(props: any) {
+    const { isLoading, user } = useAuth();
+
+    if (isLoading) {
+      return (
+        <div className="flex justify-center items-center col-span-4 h-screen">
+          <Oval stroke="#2c3e50" height="64" width="64" />
+        </div>
+      );
+    }
+
+    //authentication logic
+    if (!user) {
+      setTimeout(toast.error("Please login to view this.", {
+        style: {
+          border: " 1px solid #2c3e50",
+          padding: ".5rem",
+        },
+        iconTheme: {
+          primary: "#2c3e50",
+          secondary: "#FFFAEE",
+        },
+      }),500)
+      return <Navigate to={"/login"} />;
+    }
+
+    //role based access
+    if(roles && !roles.includes(user.role)){
+      setTimeout(toast.error("Unauthorised. Please try with a different account.", {
+        style: {
+          border: " 1px solid #2c3e50",
+          padding: ".5rem",
+        },
+        iconTheme: {
+          primary: "#2c3e50",
+          secondary: "#FFFAEE",
+        },
+      }),500)
+      return <Navigate to={'/login'}/>
+    }
+    return <Component {...props} />;
+  };
+}
