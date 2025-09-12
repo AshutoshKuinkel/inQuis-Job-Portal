698b4989bec49d5336e220a4ab0e1bdc4275a4b3
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-12 15:02:12 +1000

    detail section responsive for jobs page

--- Frontend/src/components/list jobs page/job cards/index.tsx
+++ Frontend/src/components/list jobs page/job cards/index.tsx
@@ -6,12 +6,15 @@ import { IJob } from "../../../types/job.types";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { Oval } from "react-loading-icons";
 import { GoArrowLeft, GoArrowRight } from "react-icons/go";
+import { useEffect, useState } from "react";
 
 const JobDisplay = () => {
   const navigate = useNavigate();
   const { id: selectedJobId } = useParams();
 
   const [searchParam, setSearchParam] = useSearchParams();
+  const [isMobile,setIsMobile] = useState(true)
+  const [isJobClicked,setIsJobClicked] = useState(false)
 
   const query = searchParam.get("query") || "";
   const location = searchParam.get("location") || "";
@@ -23,10 +26,26 @@ const JobDisplay = () => {
     queryKey: ["get_all_jobs", query, location, currentPage,sortBy],
   });
 
+  // useEffect for checking screen width using window.innerwidth
+  useEffect(()=>{
+    const handleScreenWidth = ()=>{
+      setIsMobile(window.innerWidth < 1024)
+    }
+
+    window.addEventListener('resize',handleScreenWidth)
+    handleScreenWidth()
+
+    return ()=> window.removeEventListener('resize',handleScreenWidth)
+  },[])
+
   const handleJobClick = (
     id: string,
   ) => {
     navigate(`/jobs/${id}?query=${query}&location=${location}&currentPage=${currentPage}&sortBy=${sortBy}`);
+    if(isMobile){
+      setIsJobClicked(true)
+      window.scrollTo(0,0)
+    }
   };
 
   const handlePage = (pageNumber: number) => {
@@ -47,10 +66,10 @@ const JobDisplay = () => {
 
   return (
     <div className="flex justify-center mt-16">
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
         {/* Cards */}
         <div className="col-span-1">
-          <div className="flex flex-col gap-3">
+          <div className={`flex flex-col gap-3 ${isMobile && isJobClicked ? 'hidden' : ''}`}>
             {isLoading && (
               <div className="flex justify-center items-center col-span-4 h-[300px]">
                 <Oval stroke="#2c3e50" height="64" width="64" />
@@ -74,13 +93,13 @@ const JobDisplay = () => {
           </div>
         </div>
 
-        {/* Detail Section */}
+        {/* Detail Section {Mobile} */}
         {isLoading ? (
           ""
-        ) : (
-          <div className="col-span-2 border border-gray-300 rounded-md max-w-4xl h-screen sticky top-0 mb-10">
+        ) : ( isMobile && isJobClicked &&
+          <div className={`col-span-2 border border-gray-300 rounded-md max-w-4xl h-screen sticky top-0 mb-10`}>
             <div
-              className="h-full overflow-hidden hover:overflow-auto"
+              className="h-full overflow-auto hover:overflow-auto"
               style={{ scrollbarGutter: "stable" }}
             >
               <DetailCard jobId={selectedJobId ?? null} />
@@ -88,8 +107,67 @@ const JobDisplay = () => {
           </div>
         )}
 
-        {/* Next Previous Buttons */}
-        {!isLoading && (
+        {/* Detail Section {Laptop} */}
+        {isLoading ? (
+          ""
+        ) : (!isMobile &&
+          <div className={`lg:block col-span-2 border border-gray-300 rounded-md max-w-4xl h-screen sticky top-0 mb-10`}>
+            <div
+              className="h-full overflow-auto hover:overflow-auto"
+              style={{ scrollbarGutter: "stable" }}
+            >
+              <DetailCard jobId={selectedJobId ?? null} />
+            </div>
+          </div>
+        )}
+
+        {/* Next Previous Buttons {Mobile}*/}
+        {!isLoading && isMobile && !isJobClicked && (
+          <div className="flex justify-between items-center pb-10 p-3">
+            <div
+              className={`flex items-center border border-[#2c3e50] p-2 space-x-2 rounded-lg text-center ${
+                currentPage === 1
+                  ? "opacity-50 cursor-not-allowed"
+                  : "cursor-pointer"
+              }`}
+              onClick={() => handlePage(currentPage - 1)}
+            >
+              <GoArrowLeft />
+              <button
+                className={`${
+                  currentPage === 1 ? "cursor-not-allowed" : "cursor-pointer"
+                }`}
+              >
+                Previous
+              </button>
+            </div>
+
+            <div
+              className={`flex items-center border border-[#2c3e50] p-2 space-x-2 rounded-lg text-center ${
+                data?.pagination?.total_pages &&
+                currentPage >= data.pagination.total_pages
+                  ? "opacity-50 cursor-not-allowed"
+                  : "cursor-pointer"
+              }`}
+              onClick={() => handlePage(currentPage + 1)}
+            >
+              <button
+                className={`${
+                  data?.pagination?.total_pages &&
+                  currentPage >= data.pagination.total_pages
+                    ? "cursor-not-allowed"
+                    : "cursor-pointer"
+                }`}
+              >
+                Next
+              </button>
+              <GoArrowRight />
+            </div>
+          </div>
+        )}
+
+        {/* Next Previous Buttons {Desktop} */}
+                {!isLoading && !isMobile && (
           <div className="flex justify-between items-center pb-10 p-3">
             <div
               className={`flex items-center border border-[#2c3e50] p-2 space-x-2 rounded-lg text-center ${
