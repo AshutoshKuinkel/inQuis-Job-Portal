d1da242ec623782933da27c3a014b503e2e79560
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-16 16:58:07 +1000

    removed service from nodemailer

--- Backend/build/utils/nodemailer.utils.js
+++ Backend/build/utils/nodemailer.utils.js
@@ -57,7 +57,7 @@ const transporter = nodemailer_1.default.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: Number(process.env.SMTP_PORT) === 465 ? true : false,
-    service: process.env.SMTP_SERVICE,
+    // service: process.env.SMTP_SERVICE,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
--- Backend/src/utils/nodemailer.utils.ts
+++ Backend/src/utils/nodemailer.utils.ts
@@ -60,7 +60,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: Number(process.env.SMTP_PORT) === 465 ? true : false,
-  service: process.env.SMTP_SERVICE,
+  // service: process.env.SMTP_SERVICE,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
