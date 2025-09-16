3e0c6cb0128a547f34d5d301580dbbc8ad03dbc4
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-16 16:21:25 +1000

    port 587 + secure false

--- Backend/build/utils/nodemailer.utils.js
+++ Backend/build/utils/nodemailer.utils.js
@@ -9,8 +9,8 @@ require("dotenv/config");
 const error_handler_middleware_1 = __importDefault(require("../middlewares/error-handler.middleware"));
 const transporter = nodemailer_1.default.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: Number(process.env.SMTP_PORT) === 465 ? true : false,
+    port: 587,
+    secure: false,
     service: process.env.SMTP_SERVICE,
     auth: {
         user: process.env.SMTP_USER,
--- Backend/src/utils/nodemailer.utils.ts
+++ Backend/src/utils/nodemailer.utils.ts
@@ -4,8 +4,8 @@ import CustomError from '../middlewares/error-handler.middleware';
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port:Number(process.env.SMTP_PORT),
-  secure:Number(process.env.SMTP_PORT) === 465 ? true : false,
+  port:587,
+  secure:false,
   service:process.env.SMTP_SERVICE,
   auth:{
     user:process.env.SMTP_USER,
