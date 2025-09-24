c88931f60697a660106974c351d9542696666792
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-24 15:34:21 +1000

    routes changed

--- Backend/src/controllers/application.controller.ts
+++ Backend/src/controllers/application.controller.ts
@@ -489,12 +489,9 @@ export const updateApplicationStatus = async (
       throw new CustomError("Job not found", 404);
     }
 
-    console.log(req.user._id.toString())
-    console.log(job.postedBy.toString())
-
-    // if (req.user._id.toString() !== job.postedBy.toString()) {
-    //   throw new CustomError("Unauthorized. Access Denied.", 403);
-    // }
+    if (req.user._id.toString() !== job.postedBy.toString()) {
+      throw new CustomError("Unauthorized. Access Denied.", 403);
+    }
 
     // Update application directly
     const application = await Application.findOneAndUpdate(
