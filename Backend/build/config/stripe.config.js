1e77ea24cf51a3e3830d08f1fdedcb974203066e
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-09-28 17:10:16 +1000

    forgot to load dotenv so api couldnt be accessed

--- Backend/build/config/stripe.config.js
+++ Backend/build/config/stripe.config.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.stripe = void 0;
 const stripe_1 = __importDefault(require("stripe"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 exports.stripe = new stripe_1.default(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2025-08-27.basil",
 });
